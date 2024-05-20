b1136ab6de8441b1fe415dde2166090998435149
jbzdarkid <jbzdarkid@gmail.com>
2024-05-20 14:35:48 -0700

    Tests passing

--- tests.py
+++ tests.py
@@ -15,66 +15,65 @@ def get_id():
 
 class Tests:
   dow_opens = {
-      '2024-05-07': 38858.94,
-      '2024-05-06': 38762.43,
-      '2024-05-03': 38709.36,
-      '2024-05-02': 38075.65,
-      '2024-05-01': 37845.56,
-      '2024-04-30': 38337.40,
-      '2024-04-29': 38282.16,
-      '2024-04-26': 38114.70,
-      '2024-04-25': 38052.09,
-      '2024-04-24': 38552.79,
-      '2024-04-23': 38356.07,
-      '2024-04-22': 38116.89,
+      '2024-05-07': '38858.94',
+      '2024-05-06': '38762.43',
+      '2024-05-03': '38709.36',
+      '2024-05-02': '38075.65',
+      '2024-05-01': '37845.56',
+      '2024-04-30': '38337.40',
+      '2024-04-29': '38282.16',
+      '2024-04-26': '38114.70',
+      '2024-04-25': '38052.09',
+      '2024-04-24': '38552.79',
+      '2024-04-23': '38356.07',
+      '2024-04-22': '38116.89',
     }
 
   #############
   #!# Tests #!#
   #############
   def test_hashes(self):
-    geohashes = main.get_geohashes(self.dow_opens, datetime.datetime(year=2024, month=5, day=07))
+    geohashes = main.get_geohashes(self.dow_opens, datetime.datetime(year=2024, month=5, day=7))
     # These values were independently confirmed with geohashing.info
-    lat, long, cent = geohashes['2024-04-30')
-    assert lat == '90233'
-    assert long == '55186'
+    lat, long, cent = geohashes['2024-04-30']
+    assert lat == '9023250203492802'
+    assert long == '5518558190767081'
     assert cent == '95'
 
-    lat, long, cent = geohashes['2024-05-01')
+    lat, long, cent = geohashes['2024-05-01']
     assert lat == '9725225494237593'
     assert long == '987217588117626'
     assert cent == '99'
 
-    lat, long, cent = geohashes['2024-05-04')
-    assert lat == '86571'
-    assert long == '46902'
+    lat, long, cent = geohashes['2024-05-04']
+    assert lat == '8657127823310143'
+    assert long == '4690159903840444'
     assert cent == '84'
 
 if __name__ == '__main__':
   test_class = Tests()
-  with patch('dow_jones.get_dow_jones_opens', new=test_class.mock_get_dow):
 
-    def is_test(method):
-      return inspect.ismethod(method) and method.__name__.startswith('test')
-    tests = list(inspect.getmembers(test_class, is_test))
-    tests.sort(key=lambda func: func[1].__code__.co_firstlineno)
+  def is_test(method):
+    return inspect.ismethod(method) and method.__name__.startswith('test')
+  tests = list(inspect.getmembers(test_class, is_test))
+  tests.sort(key=lambda func: func[1].__code__.co_firstlineno)
 
-    for test in tests:
-      if len(sys.argv) > 1: # Requested specific test(s)
-        if test[0] not in sys.argv[1:]:
-          continue
+  for test in tests:
+    if len(sys.argv) > 1: # Requested specific test(s)
+      if test[0] not in sys.argv[1:]:
+        continue
 
-      # Test setup
+    # Test setup
 
-      # Run test
-      print('---', test[0], 'started')
-      try:
-        test[1]()
-      except Exception:
-        print('!!!', test[0], 'failed:')
-        import traceback
-        traceback.print_exc()
-        sys.exit(-1)
+    # Run test
+    print('---', test[0], 'started')
+    try:
+      test[1]()
+    except Exception:
+      print('!!!', test[0], 'failed:')
+      import traceback
+      traceback.print_exc()
+      sys.exit(-1)
 
     print('===', test[0], 'passed')
   print('\nAll tests passed')
