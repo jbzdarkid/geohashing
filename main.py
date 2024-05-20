e24c447908e7036389106113a49a4707596f56c4
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2024-05-20 14:09:56 -0700

    Oh yeah, it's all coming together

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
-import collections
 import datetime
 import hashlib
 import os
+import time
+import zoneinfo
 from importlib import import_module
 Wiki = import_module('TFWiki-scripts.wikitools.wiki').Wiki
 Page = import_module('TFWiki-scripts.wikitools.page').Page
 
 import dow_jones
 
-verbose = True
+verbose = False
 
-def get_geohashes():
+def get_geohashes(dow_opens):
   geohashes = {}
-  for day, open in dow_jones.get_dow_jones_opens():
-    date = day.strftime('%Y-%m-%d')
 
-  hash = hashlib.md5(f'{date}-{dow_jones_open}'.encode('utf-8')).hexdigest()
-  if verbose:
-    print(f'Raw hash for {day}: {hash}')
+  last_dow_open = None
+  date_range = [datetime.datetime.today() + datetime.timedelta(days=i) for i in range(7)][::-1]
+  for day in date_range:
+    date = day.strftime('%Y-%m-%d'))
+    if dow_open := dow_opens.get(date):
+      last_dow_open = dow_open
+    if not last_dow_open:
+      continue # No data found, initial date may be a weekend
 
-  latitude  = str(float.fromhex(f'0.{hash[:16]}'))[2:] # Convert hex to float then removing leading '0.'
-  longitude = str(float.fromhex(f'0.{hash[16:]}'))[2:] # Convert hex to float then removing leading '0.'
-  centicule = latitude[0] + longitude[0]
-  if verbose:
-    print(f'(lat, long, cent): {latitude}, {longitude}, {centicule}')
+    if verbose:
+      print('Last dow open:', last_dow_open)
+    
+    hash = hashlib.md5(f'{date}-{last_dow_open}'.encode('utf-8')).hexdigest()
+    if verbose:
+      print(f'Raw hash for {day}: {hash}')
 
-  return (latitude, longitude, centicule)
+    latitude  = str(float.fromhex(f'0.{hash[:16]}'))[2:] # Convert hex to float then removing leading '0.'
+    longitude = str(float.fromhex(f'0.{hash[16:]}'))[2:] # Convert hex to float then removing leading '0.'
+    centicule = latitude[0] + longitude[0]
+    geohashes[date] = (latitude, longitude, centicule)
+    if verbose:
+      print(f'(lat, long, cent): {latitude}, {longitude}, {centicule}')
+
+  return geohashes
 
 DAY_OF_WEEK = 'monday, tuesday, wednesday, thursday, friday, saturday, sunday'.split(', ')
 def parse_config(contents):
-  config = [collections.defaultdict({}) # Nested map, day-of-week:(lat, long, cent):{notification_methods}
+  config = {} # Nested map, day-of-week:centicule:{data}
 
   lines = contents.split('\n')
   for line in lines:
@@ -54,11 +66,11 @@ def parse_config(contents):
       target_days = DAY_OF_WEEK # If not specified, all the days of the week
 
     for day in target_days:
-      config[day] = collections.defaultdict({})
+      config[day] = {}
       for cent in cents:
-        config[day][(lat, long, cent)] = collections.defaultdict({})
+        config[day][cent] = {'lat': lat, 'long': long}
         for method in notification_methods:
-          config[day][(lat, long, cent)[method] = True
+          config[day][cent][method] = True
   
   return config
 
@@ -75,85 +87,103 @@ def main(w):
   else:
     pages = w.get_all_category_pages('Category:Tracked by DarkBOT', namespaces=['User'])
 
-  eastern_time = datetime.timezone(-datetime.timedelta(hours=5, minutes=30))
+  eastern_time = zoneinfo.ZoneInfo('America/New_York')
   today = datetime.datetime.now(tz=eastern_time)
-  # if long >= -30: # This needs to move into the config parse.
+  # if long >= -30: # TODO: This needs to move into the config parse. Somehow.
   #   today -= datetime.timedelta(days=1)
 
   if today.weekday() in [0, 1, 2, 3]:
-    days = [today] # Monday through Thursday only report for the current day
+    days = [today]
+    if verbose:
+      print(f'{DAY_OF_WEEK[today.weekday()]} (index {today.weekday()}) is a weekday, running a single update')
   elif today.weekday() == 4:
     # Fridays update the entire weekend (since it's known by that point)
     # In theory we could update monday if it's a DOW holiday -- but that's really complicated logic and I don't want to do it.
+    # I could also just parse the NYSE holiday hours webpage.
     days = [today, today + datetime.timedelta(days=1), today + datetime.timedelta(days=2)]
+    if verbose:
+      print('Today is a Friday, so running a triple update for the weekend.')
   elif today.weekday() in [5, 6]:
-    continue # On Saturday and Sunday, no updates (because we already updated on Friday)
+    if verbose:
+      print('Today is a weekend, so the geohashes were already computed Friday.')
+    return
 
-  geohashes = get_geohashes()
+  # The Dow Jones Industrial Average opens with the New York Stock Exchange at 9:30 AM, Eastern Time.
+  # The reporting for the value is usually available within the hour, so if there's no data available by noon,
+  # assume it's a weekend or a holiday. See https://www.nyse.com/markets/hours-calendars for more precise holiday info.
+  while 1:
+    today = datetime.datetime.now(tz=eastern_time)
+    dow_opens = dow_jones.get_dow_jones_opens() # This samples 3 websites, and only reports data if >= 2 of them agree.
+    if today.strftime('%Y-%m-%d') in dow_opens:
+      if verbose:
+        print('DJI opening data found at', today)
+      break
+    elif today.hour >= 12:
+      if verbose:
+        print('DJI opening data not found by noon, assuming weekend/holiday')
+      break
+    time.sleep(60) # Sleep for 60 seconds
+
+  geohashes = get_geohashes(dow_opens) # Map of YYYY-mm-dd:(latitude, longitude, centicule).
 
   for page in pages:
-    try:
+    if verbose:
+      print(f'Handling {page.title}...')
+    config_contents = []
+    talk_contents = []
+    email_message = []
+
+    config = parse_config(page.get_wiki_text())
+    for day in days:
+      day_name = DAY_OF_WEEK[day.weekday()]
+      (latitude, longitude, centicule) = geohashes[day.strftime('%Y-%m-%d')]
+      if centicule in config[day_name]: # Centicule is tracked by config
+        data = config[day_name][centicule]
+        lat = data['lat']
+        long = data['long']
+        if verbose:
+          print(f'Found geohash on {day} within centicules for {page.title}: {lat, long, cent}')
+
+        date = day.strftime('%Y-%m-%d')
+        expedition = Page(w, f'{date} {lat} {long}')
+        map_link = f'https://maps.google.com/?q={lat}.{latitude},{long}.{longitude}'
+
+        if data.get('config_page'):
+          config_contents.append(f'\n=== [{expedition.get_edit_url()} {expedition.title}] ===')
+          config_contents.append(f'[{map_link} Centicule {centicule}]')
+
+        if data.get('talkpage'):
+          talk_contents.append(f'\n== New geohashing site on {date} ==')
+          talk_contents.append(f'See [[{page}]]')
+
+        if data.get('email'):
+          email_message.append(f'<h2>New geohashing site on {date}, in centicule {centicule}</h2>')
+          email_message.append(f'Map link: <a href="{map_link}">{map_link}</a>')
+          email_message.append(f'Config page: <a href="{page.get_page_url()}">{page.title}</a>')
+          email_message.append(f'Expedition page: <a href="{expedition.get_edit_url()}">{expedition.title}</a>')
+
+    # End 'for day in days'
+    if config_contents:
+      config = page.get_wiki_text()
+      config += '\n'.join(config_contents)
+      r = page.edit(config, bot=True, summary='Automatic update via https://github.com/jbzdarkid/geohashing'):
       if verbose:
-        print(f'Handling {page.title}...')
-      config_contents = []
-      talk_contents = []
-      email_message = []
-
-      config = parse_config(page.get_wiki_text())
-      for day in days:
-        day_name = DAY_OF_WEEK[day.weekday()]
-        for k, notification_methods in config[day_name].items()
-          lat, long, cent = k
-          if (lat, long, cent) != (latitude, longitude, centicule):
-            continue # Not tracked by config
-
-          if verbose:
-            print(f'Found geohash on {day} within centicules for {page.title}: {lat, long, cent}')
-
-          date = day.strftime('%Y-%m-%d')
-          expedition = Page(w, f'{date} {lat} {long}')
-          map_link = f'https://maps.google.com/?q={lat}.{latitude},{long}.{longitude}'
-
-          if notification_methods.get('config_page'):
-            config_contents.append(f'\n=== [{expedition.get_edit_url()} {expedition.title}] ===')
-            config_contents.append(f'[{map_link} Centicule {centicule}]')
-
-          if notification_methods.get('talkpage'):
-            talk_contents.append(f'\n== New geohashing site on {date} ==')
-            talk_contents.append(f'See [[{page}]]')
-
-          if notification_methods.get('email'):
-            email_message.append(f'<h2>New geohashing site on {date}, in centicule {centicule}</h2>')
-            email_message.append(f'Map link: <a href="{map_link}">{map_link}</a>')
-            email_message.append(f'Config page: <a href="{page.get_page_url()}">{page.title}</a>')
-            email_message.append(f'Expedition page: <a href="{expedition.get_edit_url()}">{expedition.title}</a>')
-
-      # End 'for day in days'
-      if config_contents:
-        config = page.get_wiki_text()
-        config += '\n'.join(config_contents)
-        r = page.edit(config, bot=True, summary='Automatic update via https://github.com/jbzdarkid/geohashing'):
-        if verbose:
-          print(f'Edited config page {page}: {r}')
-      if talk_contents:
-        talkpage_title = page.basename.split('/', 1)[0] # User:Darkid/Foo -> User:Darkid
-        talkpage = Page(w, talkpage_title.replace('User:', 'User talk:'))
-        talk = talkpage.get_wiki_text()
-        talk += '\n'.join(talk_contents)
-        r - talkpage.edit(talk, bot=True, summary='New geohash(es) in your centicule(s)')
-        if verbose:
-          print(f'Edited talkpage {talkpage}: {r}')
-      if email_message:
-        user = page.basename.split('/', 1)[0] # User:Darkid/Foo -> User:Darkid
-        title = 'New geohash(es) in your centicule(s)')
-        r = w.email_user(user, title, '<br>'.join(email_message))
-        if verbose:
-          print(f'Sent email to {user}: {r}')
-    except:
+        print(f'Edited config page {page}: {r}')
+    if talk_contents:
+      talkpage_title = page.basename.split('/', 1)[0] # User:Darkid/Foo -> User:Darkid
+      talkpage = Page(w, talkpage_title.replace('User:', 'User talk:'))
+      talk = talkpage.get_wiki_text()
+      talk += '\n'.join(talk_contents)
+      r - talkpage.edit(talk, bot=True, summary='New geohash(es) in your centicule(s)')
       if verbose:
-        print(f'Failed to update {page.title}')
-        import traceback
-        traceback.print_exc()
+        print(f'Edited talkpage {talkpage}: {r}')
+    if email_message:
+      user = page.basename.split('/', 1)[0] # User:Darkid/Foo -> User:Darkid
+      title = 'New geohash(es) in your centicule(s)')
+      email = '<br>'.join(email_message)
+      r = w.email_user(user, title, email)
+      if verbose:
+        print(f'Sent email to {user}: {r}')
 
 if __name__ == '__main__':
   verbose = True
